0d6220bbfa08e9f98e0583835cad37d80a038ede
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2022-02-16 14:16:22 +0100

    delete comment

--- src/entities/Updoot.ts
+++ src/entities/Updoot.ts
@@ -3,12 +3,6 @@ import { BaseEntity, Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
 import { Post } from "./Post";
 import { User } from "./User";
 
-// m to n
-// many to many
-// user <-> posts
-// user -> join table <- posts
-// user -> updoot <- posts
-
 @ObjectType()
 @Entity()
 export class Updoot extends BaseEntity {
@@ -32,24 +26,3 @@ export class Updoot extends BaseEntity {
   @ManyToOne(() => Post, (post) => post.updoots, { onDelete: "CASCADE" })
   post: Post;
 }
-
-//MIKROORM
-// @ObjectType()
-// @Entity()
-// export class Post {
-//   @Field(() => String)
-//   @PrimaryKey({ type: "uuid" })
-//   id = v4();
-
-//   @Field(() => String)
-//   @Property({ type: "date" })
-//   createdAt = new Date();
-
-//   @Field(() => String)
-//   @Property({ type: "date", onUpdate: () => new Date() })
-//   updatedAt = new Date();
-
-//   @Field()
-//   @Property({ type: "text" })
-//   title!: string;
-// }
